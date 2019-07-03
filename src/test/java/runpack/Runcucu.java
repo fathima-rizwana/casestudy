f0e130d8e1d344a0d59dfb0b8bc2dd57a48b7e8a
fathima-rizwana <40071193+fathima-rizwana@users.noreply.github.com>
2019-07-03 17:51:36 +0530

    Update Runcucu.java

--- src/test/java/runpack/Runcucu.java
+++ src/test/java/runpack/Runcucu.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"src/test/resources/register.feature"},
-                 glue="stepdefn",plugin="json:target/jsonreport1.json")
+                 glue="stepdefn",plugin="json:target/jsonreport.json")
 public class Runcucu {
 
 }
