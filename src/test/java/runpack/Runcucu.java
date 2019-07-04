7a6806a1d7841e06852818acc68001920802a598
fathima-rizwana <40071193+fathima-rizwana@users.noreply.github.com>
2019-07-04 09:24:13 +0530

    Update Runcucu.java

--- src/test/java/runpack/Runcucu.java
+++ src/test/java/runpack/Runcucu.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/resources/register.feature"},
+@CucumberOptions(features={"src/test/resources/register.feature","src/test/resorces/login.feature","src/test/resources/search.feature","src/test/resources/cart.feature"},
                  glue="stepdefn",plugin="json:target/jsonreport.json")
 public class Runcucu {
 
