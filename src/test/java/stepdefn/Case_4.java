5257669b8233830f26ee7946727584b295ce563e
fathima-rizwana <40071193+fathima-rizwana@users.noreply.github.com>
2019-07-04 09:25:28 +0530

    Update Case_4.java

--- src/test/java/stepdefn/Case_4.java
+++ src/test/java/stepdefn/Case_4.java
@@ -21,7 +21,7 @@ public class Case_4 {
 		driver.get("http://10.232.237.143:443/TestMeApp/fetchcat.htm");
 		driver.findElement(By.xpath("//*[@id='header']/div[1]/div/div/div[2]/div/ul/li[1]/a")).click(); 
 		
-		driver.findElement(By.name("userName")).sendKeys("shwesam");
+		driver.findElement(By.name("userName")).sendKeys("lalitha");
 		driver.findElement(By.name("password")).sendKeys("password123");
 		driver.findElement(By.name("Login")).click();
 	}
